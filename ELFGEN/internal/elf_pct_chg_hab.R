142647387263b89a7e70107d50011ca1993d86b0
Kleiner <Joseph.Kleiner@deq.virginia.gov>
2018-03-30 13:40:38 -0400

    adding jdkleiner_Habitat branch

---
+++ ELFGEN/internal/elf_pct_chg_hab.R
@@ -0,0 +1,90 @@
+elf_pct_chg_hab <- function(pct_inputs = list()){
+  ruslope <- pct_inputs$ruslope
+  ruint <- pct_inputs$ruint
+  biometric_title <- pct_inputs$biometric_title 
+  flow_title <- pct_inputs$flow_title
+  Feature.Name <- pct_inputs$Feature.Name
+  pct_chg <- pct_inputs$pct_chg
+  sampres <- pct_inputs$sampres
+  startdate <- pct_inputs$startdate
+  enddate <- pct_inputs$enddate
+  
+  search_code <- pct_inputs$search_code
+  x_metric <- pct_inputs$x_metric
+  y_metric <- pct_inputs$y_metric
+
+  its <- seq(1, 500, 0.01)
+  xvalues = c(its)
+  sa <- (ruslope*log(its))+ruint
+  
+  pct_list <- c(5,10,20,30,40,50)
+  slope_table <- data.frame(xvalues=xvalues,
+                            stringsAsFactors=FALSE)
+  
+  #i <- 1
+  for (i in 1:length(pct_list)) {
+    pct <- (its -((pct_list[i]/100)*its))
+    sb <- (ruslope*log(pct))+ruint
+    pct_chgb <- (((sa-sb)/sa)*100)
+    pct_chgs = c(pct_chgb)
+    slope_table_i = data.frame(pct_chgs)
+    names(slope_table_i) <- c(paste("pct_chg_",pct_list[i],sep=""))
+    slope_table <- cbind(slope_table, slope_table_i)
+  }
+  
+  slope_table_export <- slope_table
+  names(slope_table_export)[1]<-x_metric
+  slope_table_export <- data.frame(slope_table_export[1],
+                             sa,
+                             slope_table_export[2],
+                             slope_table_export[3],
+                             slope_table_export[4],
+                             slope_table_export[5],
+                             slope_table_export[6],
+                             slope_table_export[7]
+  )
+  names(slope_table_export)[2]<-y_metric
+  write.csv(slope_table_export, file = paste(save_directory,"\\pctchg_",search_code,"_",y_metric,".csv",sep=""))
+  
+  
+title_projname <- sampres
+
+#Plot titles
+ptitle <- paste("Change in ",biometric_title," at Various % Flow Reductions","\n", Feature.Name," (",startdate," to ",enddate,")\n",title_projname," grouping",sep="")
+xaxis_title <- paste("\n",flow_title,sep="");
+yaxis_title <- paste("% Decrease in ", biometric_title,"\n", sep="");
+
+plt2 <- ggplot(slope_table, aes(x=xvalues, y=pct_chgs_20)) + 
+  
+  geom_line(data = slope_table, aes(x=xvalues,y=pct_chg_50,color = "black")) + 
+  geom_line(data = slope_table, aes(x=xvalues,y=pct_chg_40,color = "blue")) + 
+  geom_line(data = slope_table, aes(x=xvalues,y=pct_chg_30,color = "green"))+
+  geom_line(data = slope_table, aes(x=xvalues,y=pct_chg_20,color = "red"))+
+  geom_line(data = slope_table, aes(x=xvalues,y=pct_chg_10,color = "violet"))+
+  geom_line(data = slope_table, aes(x=xvalues,y=pct_chg_5,color = "wheat"))+
+  
+  scale_color_manual(
+    "Flow Reduction",
+     values=c("black","blue","forestgreen","red","darkmagenta","sienna4"),
+     labels=c("50%","40%","30%","20%","10%","5%")
+    ) + 
+  
+  scale_x_log10(
+    #limits = c(0.1,500),
+    limits = c(1,500),
+    breaks = trans_breaks("log", function(x) {10^x})
+ #   breaks = trans_breaks("log10", function(x) {10^x})#,
+    #labels = trans_format("log10", math_format(10^.x))
+    #labels = round(x,digits = 0)
+  ) +
+  annotation_logticks(sides = "b")+
+  
+  #scale_y_continuous(limits = c(0, 100))+
+  
+  ggtitle(ptitle)+
+  labs(x=xaxis_title,y=yaxis_title)+
+  theme(axis.text.x = element_text(colour="grey20",size=15,hjust=.5,vjust=.5,face="plain"),
+        axis.text.y = element_text(colour="grey20",size=15,hjust=.5,vjust=.5,face="plain"))
+
+}
+
