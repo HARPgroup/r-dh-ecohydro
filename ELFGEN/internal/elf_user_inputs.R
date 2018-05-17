d956a01017371faeac2ee674f7234f073408aaab
Kleiner <Joseph.Kleiner@deq.virginia.gov>
2018-05-17 15:55:02 -0400

    minor user input updates

--- ELFGEN/internal/elf_user_inputs.R
+++ ELFGEN/internal/elf_user_inputs.R
@@ -7,13 +7,13 @@ datasite <- "http://deq2.bse.vt.edu/d.dh" # where to get the raw data to analyze
 #----------------------------------------------
 
 #----Change Basepath here to point to your global config file:
-basepath='/usr/local/home/git/r-dh-ecohydro';
-#basepath='C:\\Users\\nrf46657\\Desktop\\VAHydro Development\\GitHub\\r-dh-ecohydro\\';
+#basepath='/usr/local/home/git/r-dh-ecohydro';
+basepath='C:\\Users\\nrf46657\\Desktop\\VAHydro Development\\GitHub\\r-dh-ecohydro\\';
 # set your local directory paths in config.local.private located in filepath above
 # this file will NOT be sent to git, so it should persist
 # so, edit config.local.private once and you should be good to go
-source('/var/www/R/config.local.private');
-#source(paste(basepath,'config.local.private',sep='/'));
+#source('/var/www/R/config.local.private');
+source(paste(basepath,'config.local.private',sep='/'));
 
 #Load Functions               
 source(paste(fxn_locations,"elf_retrieve_data.R", sep = ""));  #loads function used to retrieve F:E data from VAHydro
@@ -31,7 +31,7 @@ source(paste(fxn_locations,"elf_default_inputs.R", sep = ""));
 # Now add custom local settings here
 inputs$x_metric = 'erom_q0001e_mean'; #Flow metric to be plotted on the x-axis
 inputs$y_metric = 'aqbio_nt_total';
-inputs$ws_ftype = c('hwi_region');
+inputs$ws_ftype = c('nhd_huc6');
 inputs$target_hydrocode = '';
 inputs$quantile = .80;
 inputs$send_to_rest = "NO";
