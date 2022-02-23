183f29253cd0daee739d3d262e258fd23b2b4683
Burgholzer <robert.burgholzer@deq.virginia.gov>
2022-02-23 17:15:41 -0500

    eliminate globals. more anaylysis examples

--- Analysis/habitat/example_ifim_wua_change_plot.R
+++ Analysis/habitat/example_ifim_wua_change_plot.R
@@ -1,5 +1,5 @@
 #----------------------------------------------
-site <- "http://deq2.bse.vt.edu/d.dh"    #Specify the site of interest, either d.bet OR d.dh
+site <- "http://deq1.bse.vt.edu:81/d.dh"    #Specify the site of interest, either d.bet OR d.dh
 #----------------------------------------------
 # Load Libraries
 library(hydrotools)
@@ -10,21 +10,27 @@ source(paste(github_location,'r-dh-ecohydro/Analysis/habitat','hab_ts_functions.
 
 
 # INPUTS #######################################################################################
-ifim_featureid <- 397295 #Potomac 8&9
-wshed_featureid <- 68346 #Potomac River Great Falls
+#ifim_featureid <- 397295 #Potomac 8&9
+#wshed_featureid <- 68346 #Potomac River Great Falls
+#ifim_featureid <- 397301 # Front Royal
+#wshed_featureid <- 68296 # SF Shenandoah River @ Front Royal
+ifim_featureid <- 397299 # Lynnwood IFIM
+wshed_featureid <- 68296 # SF Shenandoah River @ Lynnwood VA
+#ifim_featureid <- 397300 # Luray
+#wshed_featureid <- 68326 # SF Shenandoah River @ Luray
 
 ################################################################################################
 # RETRIEVE RSEG MODEL
 wshed_model <- om_get_model(base_url, wshed_featureid, 'dh_feature', 'vahydro-1.0', 'any')
-elid <- om_get_model_elementid(base_url, wshed_model$pid) 
+elid <- om_get_model_elementid(base_url, wshed_model$pid)
 
 ################################################################################################
-# RETRIEVE IFIM SITE FEATURE 
+# RETRIEVE IFIM SITE FEATURE
 ifim_site <- getFeature(list(hydroid = ifim_featureid), token, site, feature)
 ifim_site_name <- as.character(ifim_site$name)
 
 ################################################################################################
-# RETRIEVE WUA TABLE 
+# RETRIEVE WUA TABLE
 ifim_dataframe <- vahydro_prop_matrix(ifim_featureid, 'dh_feature','ifim_habitat_table')
 WUA.df <- t(ifim_dataframe)
 targets <- colnames(WUA.df)[-1]
@@ -56,7 +62,7 @@ gage <- as.character(gageprop$propcode)
 model_flows_11 <- om_get_rundata(elid, 11)
 model_flows_11$Qbaseline <- model_flows_11$Qout + (model_flows_11$wd_cumulative_mgd - model_flows_11$ps_cumulative_mgd ) * 1.547
 ts1 <- as.data.frame(model_flows_11[,c('thisdate', 'Qout')])
-ts1$thisdate <- as.character(as.Date(index(model_flows_11))) 
+ts1$thisdate <- as.character(as.Date(index(model_flows_11)))
 names(ts1) <- c('Date', 'Flow')
 ts1$Flow <- (as.numeric(ts1$Flow)*weighting_factor) #ADJUST MODEL FLOW USING WEIGHTING FACTOR
 
@@ -65,7 +71,7 @@ ts1$Flow <- (as.numeric(ts1$Flow)*weighting_factor) #ADJUST MODEL FLOW USING WEI
 model_flows_13 <- om_get_rundata(elid, 13)
 model_flows_13$Qbaseline <- model_flows_13$Qout + (model_flows_13$wd_cumulative_mgd - model_flows_13$ps_cumulative_mgd ) * 1.547
 ts2 <- as.data.frame(model_flows_13[,c('thisdate', 'Qout')])
-ts2$thisdate <- as.character(as.Date(index(model_flows_13))) 
+ts2$thisdate <- as.character(as.Date(index(model_flows_13)))
 names(ts2) <- c('Date', 'Flow')
 ts2 <- ts2
 ts2$Flow <- (as.numeric(ts2$Flow)*weighting_factor) #ADJUST MODEL FLOW USING WEIGHTING FACTOR
@@ -95,11 +101,13 @@ ggsave(paste(export_path,'ifim_boxplot_11Qout_13Qout_05_',elid,'.png',sep=""), w
 ################################################################################################
 # PLOT THE HABITAT CHANGE BETWEEN Qbaseline AND Qout FOR THE SECOND MODEL RUN
 
+runid = 11
+
 ts2base <- as.data.frame(model_flows_13[,c('thisdate', 'Qbaseline')])
-ts2base$thisdate <- as.character(as.Date(index(model_flows_13))) 
+ts2base$thisdate <- as.character(as.Date(index(model_flows_13)))
 names(ts2base) <- c('Date', 'Flow')
 
-ts2base$Flow <- (as.numeric(ts2base$Flow)*weighting_factor) 
+ts2base$Flow <- (as.numeric(ts2base$Flow)*weighting_factor)
 ifim_plot05 <- ifim_wua_change_plot(ts2base, ts2, WUA.df, 0.05,"ifim_da_sqmi" = ifim_da_sqmi,runid_a = "13",metric_a = "Qbaseline",runid_b = "13",metric_b = "Qout")
 ifim_plot05 + ylim(c(-50,50))
 ggsave(paste(export_path,'ifim_boxplot_13QQbaseline_13Qout_05_',elid,'.png',sep=""), width = 7, height = 4)
@@ -116,12 +124,41 @@ ggsave(paste(export_path,'ifim_boxplot_13QQbaseline_13Qout_20_',elid,'.png',sep=
 
 
 
-model_flows_17 <- om_get_rundata(elid, 17) 
+model_flows_17 <- om_get_rundata(elid, 17)
 ts3 <- as.data.frame(model_flows_17[,c('thisdate', 'Qout')])
-ts3$thisdate <- as.character(as.Date(index(model_flows_17))) 
+ts3$thisdate <- as.character(as.Date(index(model_flows_17)))
 names(ts3) <- c('Date', 'Flow')
 ts1cc_daterange <- sqldf("select * from ts1 where Date <= '2000-09-30' ")
 ifim_plot10cc <- ifim_wua_change_plot(ts1cc_daterange, ts3, WUA.df, 0.1,"ifim_da_sqmi" = ifim_da_sqmi,runid_a = "11",metric_a = "Qout",runid_b = "17",metric_b = "Qout")
 ifim_plot10cc + ylim(c(-50,50))
 ggsave(paste(export_path,'ifim_boxplot_11Qout_17Qout_10_',elid,'.png',sep=""), width = 7, height = 4)
 
+
+# do the how much percent decrease analysis (was the original for this )
+runid = 11
+tsrun <- om_get_rundata(elid, runid, site = omsite)
+ts1 <- as.data.frame(tsrun[,c('thisdate', 'Qout')])
+ts1$thisdate <- as.character(as.Date(index(tsrun)))
+names(ts1) <- c('Date', 'Flow')
+ts2 <- ts1
+ts2$Flow <- 0.6 * ts1$Flow
+ts3 <- ts1
+ts3$Flow <- 0.2 * ts1$Flow
+
+
+ifim_plot05 <- ifim_wua_change_plot(ts1, ts2, WUA.df, 0.05,"ifim_da_sqmi" = ifim_da_sqmi,runid_a = "11",metric_a = "Qbaseline",runid_b = "11",metric_b = "Qout")
+ifim_plot05 + ylim(c(-50,50))
+
+ifim_plot10 <- ifim_wua_change_plot(ts1, ts2, WUA.df, 0.1,"ifim_da_sqmi" = ifim_da_sqmi,runid_a = "11",metric_a = "Qbaseline",runid_b = "11",metric_b = "Qout")
+ifim_plot10 + ylim(c(-50,50))
+
+ifim_plot100 <- ifim_wua_change_plot(ts1, ts2, WUA.df, 1.0,"ifim_da_sqmi" = ifim_da_sqmi,runid_a = "11",metric_a = "Qbaseline",runid_b = "11",metric_b = "Qout")
+ifim_plot100 + ylim(c(-50,50))
+ifim_plot100
+
+ifim_plot1020 <- ifim_wua_change_plot(ts1, ts3, WUA.df, 0.5,"ifim_da_sqmi" = ifim_da_sqmi,runid_a = "11",metric_a = "Qbaseline",runid_b = "11",metric_b = "Qout")
+ifim_plot1020 + ylim(c(-50,50))
+ifim_plot1020
+om_flow_table(ts1, 'Flow')
+
+ggsave(paste(export_path,'ifim_boxplot_13QQbaseline_13Qout_05_',elid,'.png',sep=""), width = 7, height = 4)
--- Analysis/habitat/hab_ts_functions.R
+++ Analysis/habitat/hab_ts_functions.R
@@ -1,6 +1,6 @@
 library(dataRetrieval)
 
-#HABITAT FUNCTIONS 
+#HABITAT FUNCTIONS
 clean_historic <- function(historic){
   # ******************************************************************************************
   # Remove any rows with "Ice", "P Ice" or "P Eqp" values for Flow_cd
@@ -9,24 +9,24 @@ clean_historic <- function(historic){
     print("Removing Ice Flows")
     historic <- historic[-which(historic[,5]== "Ice"),]
   }
-  
+
   if (length(which(historic[,5]== "P Ice")) != 0 ){
     print("Removing Ice Flows")
     historic <- historic[-which(historic[,5]== "P Ice"),]
   }
-  
+
   if (length(which(historic[,5]== "P Eqp")) != 0 ){
     print("Removing P Eqp Flows")
     historic <- historic[-which(historic[,5]== "P Eqp"),]
   }
-  
+
   if (length(which(historic[,4] < 0.0)) != 0 ){
     print("Removing Negative Flow Values")
     historic <- historic[-which(historic[,4] < 0.0),]
   }
-  
+
   historic <- historic
-} 
+}
 f_fxn <- function(gage,factor){
 gage_data <- readNWISdv(gage,'00060') #Retrieve all historic gage streamflow data in cfs
 gage_data <- clean_historic(gage_data)
@@ -39,7 +39,7 @@ flow.ts.range_fxn <- function(f,timespan){
   flow.ts <- f[,3:4] #extract time series as the 3rd and 4th columns
   f4 <- as.numeric(f[,4]) #read 4th column of USGS data (mean daily flow)
   # Indicate desired range for time series
-  
+
   if (timespan == "all") {
     flow.ts.range <- flow.ts #entire period of record
   } else if (timespan == "current_year"){
@@ -47,9 +47,9 @@ flow.ts.range_fxn <- function(f,timespan){
   } else {
     flow.ts.range <- flow.ts[(dim(flow.ts)[1]-3650):dim(flow.ts)[1],] #most recent 10 years
   }
-  
+
 }
-wua.at.q_fxn <- function(flow.ts.range){
+wua.at.q_fxn <- function(flow.ts.range, WUA.df){
 q <- as.numeric(flow.ts.range[,2]) #extract just the flows from flow time series
 
 ### Extract all WUA values for a SERIES of flows
@@ -60,13 +60,13 @@ colnames(wua.at.q) <- targets
 
 for (i in 1:length(q)) {
   currentq <- q[i] #keep track of what flow the loop is on
-  
+
   # Check if currentq is NA
   if (is.na(currentq) == "TRUE") {
     wua.at.q[i,] <- NA
     next
   }
-  
+
   # Check if currentq is beyond range of WUA table
   min.q <- min(WUA.df[,1])
   max.q <- max(WUA.df[,1])
@@ -76,10 +76,10 @@ for (i in 1:length(q)) {
   } else if (currentq > max.q) {
     wua.at.q[i,] <- NA
     next
-  } 
-  
+  }
+
   rowkey <- which.min(abs(as.numeric(WUA.df[,1]) - currentq)) #find flow closest to desired q
-  
+
   # Determine the given flows (and row indices) that bound the desired flow
   if ((as.numeric(WUA.df[rowkey,1]) - currentq) > 0) {
     uplim.key <- which.min(abs(as.numeric(WUA.df[,1]) - currentq))
@@ -90,8 +90,8 @@ for (i in 1:length(q)) {
   } else if ((as.numeric(WUA.df[rowkey,1]) - currentq) == 0) {
     lowlim.key <- which.min(abs(as.numeric(WUA.df[,1]) - currentq))
     uplim.key <- which.min(abs(as.numeric(WUA.df[,1]) - currentq))
-  } 
-  
+  }
+
   # Interpolate (linear) btwn WUA values for bounding flows
   for (j in 1:length(targets)) {
     if (lowlim.key == uplim.key) {
@@ -99,10 +99,10 @@ for (i in 1:length(q)) {
     } else {
       #linear interpolation: Yq = Ylow + ((Yup-Ylow)*(Xq-Xlow)/(Xup-Xlow))
       Xup <- WUA.df[uplim.key, 1]
-      Yup <- WUA.df[uplim.key, j+1] 
+      Yup <- WUA.df[uplim.key, j+1]
       Xlow <- WUA.df[lowlim.key, 1]
       Ylow <- WUA.df[lowlim.key, j+1]
-      wua.at.q[i,j] <- Ylow + ((Yup-Ylow)*(currentq-Xlow)/(Xup-Xlow)) 
+      wua.at.q[i,j] <- Ylow + ((Yup-Ylow)*(currentq-Xlow)/(Xup-Xlow))
     }
   }
 }
@@ -126,7 +126,7 @@ date.label <- format(dates[interval], "%m/%d/%y")
 axis(side=1, at=dates[interval], labels=date.label, cex.axis=0.9)
 
 par(new=TRUE)
-plot(flow.ts.range[,1], as.numeric(rownames(wua.at.q)), col="black", type="l", axes=F, 
+plot(flow.ts.range[,1], as.numeric(rownames(wua.at.q)), col="black", type="l", axes=F,
      xlab=NA, ylab=NA)
 axis(side=4)
 mtext(side=4, line=3, "Flow (cfs)")
@@ -134,11 +134,11 @@ mtext(side=4, line=3, "Flow (cfs)")
 
 par(new=FALSE)
 par(mar=c(0,0,0,0))
-plot(flow.ts.range[,1], rownames(wua.at.q), type="n", axes=FALSE, 
+plot(flow.ts.range[,1], rownames(wua.at.q), type="n", axes=FALSE,
      xlab=NA, ylab=NA)
-legend("left", c(colnames(wua.at.q), "Flow"), 
-       pch=c(rep(1, dim(wua.at.q)[2]), NA), 
-       lty=c(rep(NA, dim(wua.at.q)[2]), 1), 
+legend("left", c(colnames(wua.at.q), "Flow"),
+       pch=c(rep(1, dim(wua.at.q)[2]), NA),
+       lty=c(rep(NA, dim(wua.at.q)[2]), 1),
        col=c(rainbow(dim(wua.at.q)[2]), "black"))
 
 
@@ -149,7 +149,7 @@ layout(matrix(c(1,2,3,4), 2, 2), widths=c(8,2), heights=c(2,1)) #set layout
 
 #plot habitat time series (position 1)
 par(mar=c(1, 4.1, 2, 1)) #adjust margins for first plot (bottom, left, top, right)
-matplot(flow.ts.range[,1], wua.at.q, pch=1, xaxt="n", 
+matplot(flow.ts.range[,1], wua.at.q, pch=1, xaxt="n",
         col=rainbow(dim(wua.at.q)[2]),
         xlab=NA, ylab="Weighted Usable Area (ft2/1000 ft)",
         main=paste("Habitat Time Series at ",ifim_site_name," (USGS ", gage,")", sep=""))
@@ -160,21 +160,21 @@ axis(side=1, at=dates[interval], labels=date.label, cex.axis=0.9)
 
 #plot flow time series (position 2)
 par(mar=c(4.1,4.1,1,1)) #adjust margins for second plot
-plot(flow.ts.range[,1], as.numeric(rownames(wua.at.q)), col="blue", type="l", xaxt="n", 
+plot(flow.ts.range[,1], as.numeric(rownames(wua.at.q)), col="blue", type="l", xaxt="n",
      xlab="Time", ylab="Flow (cfs)")
 axis(side=1, at=dates[interval], labels=date.label, cex.axis=0.9)
 
 #plot habitat time series legend (position 3)
 par(mar=c(0,0,0,0))
-plot(flow.ts.range[,1], rownames(wua.at.q), type="n", axes=FALSE, 
+plot(flow.ts.range[,1], rownames(wua.at.q), type="n", axes=FALSE,
      xlab=NA, ylab=NA)
-legend("left", colnames(wua.at.q), 
-       pch=1,  
+legend("left", colnames(wua.at.q),
+       pch=1,
        col=rainbow(dim(wua.at.q)[2]))
 
 #plot flow time series legend (position 4)
 par(mar=c(0,0,0,0))
-plot(flow.ts.range[,1], rownames(wua.at.q), type="n", axes=FALSE, 
+plot(flow.ts.range[,1], rownames(wua.at.q), type="n", axes=FALSE,
      xlab=NA, ylab=NA)
 #legend("left", "Flow", lty=1, col="blue") #remove...too redundant
 
@@ -185,7 +185,7 @@ dev.off()
 
 }
 hab_ts_plot_single_fxn <- function(flow.ts.range,wua.at.q,filename,ifim_site_name,ifim_metric){
-  
+
   wua.at.q <- data.frame(wua.at.q[,ifim_metric])
   colnames(wua.at.q) <- c(ifim_metric)
 
@@ -194,10 +194,10 @@ hab_ts_plot_single_fxn <- function(flow.ts.range,wua.at.q,filename,ifim_site_nam
   ### Plot flow time series BELOW habitat time series
   layout(matrix(c(1,2,3,4), 2, 2), widths=c(8,2), heights=c(2,1)) #set layout
   #layout.show(4)
-  
+
   #plot habitat time series (position 1)
   par(mar=c(1, 4.1, 2, 1)) #adjust margins for first plot (bottom, left, top, right)
-  matplot(flow.ts.range[,1], wua.at.q, pch=1, xaxt="n", 
+  matplot(flow.ts.range[,1], wua.at.q, pch=1, xaxt="n",
           col=rainbow(dim(wua.at.q)[2]),
           xlab=NA, ylab="Weighted Usable Area (ft2/1000 ft)",
           main=paste("Habitat Time Series at ",ifim_site_name," (USGS ", gage,")", sep=""))
@@ -205,51 +205,51 @@ hab_ts_plot_single_fxn <- function(flow.ts.range,wua.at.q,filename,ifim_site_nam
   interval <- seq(1, length(dates), by=90) #every 90 days
   date.label <- format(dates[interval], "%m/%d/%y")
   axis(side=1, at=dates[interval], labels=date.label, cex.axis=0.9)
-  
+
   #plot flow time series (position 2)
   par(mar=c(4.1,4.1,1,1)) #adjust margins for second plot
-  plot(flow.ts.range[,1], flow.ts.range[,2], log="y", col="blue", type="l", xaxt="n", 
+  plot(flow.ts.range[,1], flow.ts.range[,2], log="y", col="blue", type="l", xaxt="n",
        xlab="Time", ylab="Flow (cfs)")
   axis(side=1, at=dates[interval], labels=date.label, cex.axis=0.9)
-  
+
   #plot habitat time series legend (position 3)
   par(mar=c(0,0,0,0))
-  plot(flow.ts.range[,1], rownames(wua.at.q), type="n", axes=FALSE, 
+  plot(flow.ts.range[,1], rownames(wua.at.q), type="n", axes=FALSE,
        xlab=NA, ylab=NA)
-  legend("left", colnames(wua.at.q), 
-         pch=1,  
+  legend("left", colnames(wua.at.q),
+         pch=1,
          col=rainbow(dim(wua.at.q)[2]))
-  
+
   #plot flow time series legend (position 4)
   par(mar=c(0,0,0,0))
-  #plot(flow.ts.range[,1], rownames(wua.at.q), type="n", axes=FALSE, 
-  plot(flow.ts.range[,1], rownames(wua.at.q), type="n", axes=FALSE, 
+  #plot(flow.ts.range[,1], rownames(wua.at.q), type="n", axes=FALSE,
+  plot(flow.ts.range[,1], rownames(wua.at.q), type="n", axes=FALSE,
        xlab=NA, ylab=NA)
 
-  
+
 }
 hab_ts_plot_overlay_fxn <- function(flow.ts.range_0,wua.at.q_0,flow.ts.range_10,wua.at.q_10,filename,ifim_site_name,ifim_metric){
-  
+
   #print(head(flow.ts.range_0))
   #print(head(wua.at.q_0))
   #print(head(flow.ts.range_10))
   #print(head(wua.at.q_10))
-  
+
   wua.at.q_0 <- data.frame(wua.at.q_0[,ifim_metric])
   colnames(wua.at.q_0) <- c(ifim_metric)
-  
+
   wua.at.q_10 <- data.frame(wua.at.q_10[,ifim_metric])
   colnames(wua.at.q_10) <- c(ifim_metric)
-  
+
   layout(matrix(c(1,2), 1, 2), widths=c(7.5,2.5))
-  
+
   ### Plot flow time series BELOW habitat time series
   layout(matrix(c(1,2,3,4), 2, 2), widths=c(8,2), heights=c(2,1)) #set layout
   #layout.show(4)
-  
+
   #plot habitat time series (position 1)
   par(mar=c(1, 4.1, 2, 1)) #adjust margins for first plot (bottom, left, top, right)
-  matplot(flow.ts.range_0[,1], wua.at.q_0, pch=1, xaxt="n", 
+  matplot(flow.ts.range_0[,1], wua.at.q_0, pch=1, xaxt="n",
           col="blue",
           xlab=NA, ylab="Weighted Usable Area (ft2/1000 ft)",
           main=paste("Habitat Time Series at ",ifim_site_name," (USGS ", gage,")", sep=""))
@@ -261,60 +261,60 @@ hab_ts_plot_overlay_fxn <- function(flow.ts.range_0,wua.at.q_0,flow.ts.range_10,
   # OVERLAY HAB TS REDUCED BY 10%
   matplot(flow.ts.range_10[,1], wua.at.q_10, add = TRUE, pch=1, xaxt="n",col="red")
   #-------------------------------------------------------------------
-  
+
   #plot flow time series (position 2)
   par(mar=c(4.1,4.1,1,1)) #adjust margins for second plot
-  plot(flow.ts.range_0[,1], flow.ts.range_0[,2], log="y", col="blue", type="l", xaxt="n", 
+  plot(flow.ts.range_0[,1], flow.ts.range_0[,2], log="y", col="blue", type="l", xaxt="n",
        xlab="Time", ylab="Flow (cfs)")
   axis(side=1, at=dates[interval], labels=date.label, cex.axis=0.9)
   #-------------------------------------------------------------------
   # OVERLAY FLOW TS REDUCED BY 10%
   points(flow.ts.range_10[,1], flow.ts.range_10[,2], col="red", type="l", xaxt="n")
   #-------------------------------------------------------------------
-  
+
   #plot habitat time series legend (position 3)
   par(mar=c(0,0,0,0))
-  plot(flow.ts.range_0[,1], rownames(wua.at.q_0), type="n", axes=FALSE, 
+  plot(flow.ts.range_0[,1], rownames(wua.at.q_0), type="n", axes=FALSE,
        xlab=NA, ylab=NA)
-  legend("left", legend=c(colnames(wua.at.q_0),"flow reduced by 10%"), 
-         pch=1,  
+  legend("left", legend=c(colnames(wua.at.q_0),"flow reduced by 10%"),
+         pch=1,
          col=c("blue","red"))
-  
-  
+
+
   #plot flow time series legend (position 4)
   par(mar=c(0,0,0,0))
-  #plot(flow.ts.range_0[,1], rownames(wua.at.q_0), type="n", axes=FALSE, 
-  plot(flow.ts.range_0[,1], rownames(wua.at.q_0), type="n", axes=FALSE, 
+  #plot(flow.ts.range_0[,1], rownames(wua.at.q_0), type="n", axes=FALSE,
+  plot(flow.ts.range_0[,1], rownames(wua.at.q_0), type="n", axes=FALSE,
        xlab=NA, ylab=NA)
-  legend("left", legend=c("gage flow","gage flow (reduced by 10%)"), 
-         lty=1,  
+  legend("left", legend=c("gage flow","gage flow (reduced by 10%)"),
+         lty=1,
          col=c("blue","red"))
-  
+
 }
 hab_ts_plot_multi_fxn <- function(inputs,filename,ifim_site_name,ifim_metric){
-  
+
 
     flow.ts.range_0 <- inputs$flow.ts.range_0
-    wua.at.q_0 <- inputs$wua.at.q_0   
+    wua.at.q_0 <- inputs$wua.at.q_0
         wua.at.q_0 <- data.frame(wua.at.q_0[,ifim_metric])
         colnames(wua.at.q_0) <- c(ifim_metric)
     flow.ts.range_5 <- inputs$flow.ts.range_5
-    wua.at.q_5 <- inputs$wua.at.q_5  
+    wua.at.q_5 <- inputs$wua.at.q_5
         wua.at.q_5 <- data.frame(wua.at.q_5[,ifim_metric])
         colnames(wua.at.q_5) <- c(ifim_metric)
-    flow.ts.range_10 <- inputs$flow.ts.range_10                      
+    flow.ts.range_10 <- inputs$flow.ts.range_10
     wua.at.q_10 <- inputs$wua.at.q_10
         wua.at.q_10 <- data.frame(wua.at.q_10[,ifim_metric])
         colnames(wua.at.q_10) <- c(ifim_metric)
-    flow.ts.range_20 <- inputs$flow.ts.range_20                      
+    flow.ts.range_20 <- inputs$flow.ts.range_20
     wua.at.q_20 <- inputs$wua.at.q_20
         wua.at.q_20 <- data.frame(wua.at.q_20[,ifim_metric])
         colnames(wua.at.q_20) <- c(ifim_metric)
-    flow.ts.range_30 <- inputs$flow.ts.range_30                      
+    flow.ts.range_30 <- inputs$flow.ts.range_30
     wua.at.q_30 <- inputs$wua.at.q_30
         wua.at.q_30 <- data.frame(wua.at.q_30[,ifim_metric])
         colnames(wua.at.q_30) <- c(ifim_metric)
-    flow.ts.range_40 <- inputs$flow.ts.range_40                     
+    flow.ts.range_40 <- inputs$flow.ts.range_40
     wua.at.q_40 <- inputs$wua.at.q_40
         wua.at.q_40 <- data.frame(wua.at.q_40[,ifim_metric])
         colnames(wua.at.q_40) <- c(ifim_metric)
@@ -322,18 +322,18 @@ hab_ts_plot_multi_fxn <- function(inputs,filename,ifim_site_name,ifim_metric){
     wua.at.q_50 <- inputs$wua.at.q_50
         wua.at.q_50 <- data.frame(wua.at.q_50[,ifim_metric])
         colnames(wua.at.q_50) <- c(ifim_metric)
-  
 
-  
+
+
   layout(matrix(c(1,2), 1, 2), widths=c(7.5,2.5))
-  
+
   ### Plot flow time series BELOW habitat time series
   layout(matrix(c(1,2,3,4), 2, 2), widths=c(8,2), heights=c(2,1)) #set layout
   #layout.show(4)
-  
+
   #plot habitat time series (position 1)
   par(mar=c(1, 4.1, 2, 1)) #adjust margins for first plot (bottom, left, top, right)
-  matplot(flow.ts.range_0[,1], wua.at.q_0, pch=1, xaxt="n", 
+  matplot(flow.ts.range_0[,1], wua.at.q_0, pch=1, xaxt="n",
           col="blue",
           xlab=NA, ylab="Weighted Usable Area (ft2/1000 ft)",
           main=paste("Habitat Time Series at ",ifim_site_name," (USGS ", gage,")", sep=""))
@@ -349,7 +349,7 @@ hab_ts_plot_multi_fxn <- function(inputs,filename,ifim_site_name,ifim_metric){
   matplot(flow.ts.range_30[,1], wua.at.q_30, add = TRUE, pch=1, xaxt="n",col="orange")
   matplot(flow.ts.range_40[,1], wua.at.q_40, add = TRUE, pch=1, xaxt="n",col="black")
   matplot(flow.ts.range_50[,1], wua.at.q_50, add = TRUE, pch=1, xaxt="n",col="violet")
-  
+
   # OVERLAY HAB TS REDUCED BY 10% (LINES)
   #matplot(flow.ts.range_5[,1], wua.at.q_5, add = TRUE, type="l", xaxt="n",col="firebrick")
   #matplot(flow.ts.range_10[,1], wua.at.q_10, add = TRUE, type="l", xaxt="n",col="blue")
@@ -359,10 +359,10 @@ hab_ts_plot_multi_fxn <- function(inputs,filename,ifim_site_name,ifim_metric){
   #matplot(flow.ts.range_50[,1], wua.at.q_50, add = TRUE, type="l", xaxt="n",col="violet")
 
   #-------------------------------------------------------------------
-  
+
   #plot flow time series (position 2)
   par(mar=c(4.1,4.1,1,1)) #adjust margins for second plot
-  plot(flow.ts.range_0[,1], flow.ts.range_0[,2], log="y", col="blue", type="l", xaxt="n", 
+  plot(flow.ts.range_0[,1], flow.ts.range_0[,2], log="y", col="blue", type="l", xaxt="n",
        xlab="Time", ylab="Flow (cfs)")
   axis(side=1, at=dates[interval], labels=date.label, cex.axis=0.9)
   #-------------------------------------------------------------------
@@ -375,11 +375,11 @@ hab_ts_plot_multi_fxn <- function(inputs,filename,ifim_site_name,ifim_metric){
   points(flow.ts.range_50[,1], flow.ts.range_50[,2], col="violet", type="l", xaxt="n")
   #-------------------------------------------------------------------
 
-  
-  
+
+
   #plot habitat time series legend (position 3)
   par(mar=c(0,0,0,0))
-  plot(flow.ts.range_0[,1], rownames(wua.at.q_0), type="n", axes=FALSE, 
+  plot(flow.ts.range_0[,1], rownames(wua.at.q_0), type="n", axes=FALSE,
        xlab=NA, ylab=NA)
   legend("left", legend=c(colnames(wua.at.q_0),
                           "flow reduced by 5%",
@@ -388,8 +388,8 @@ hab_ts_plot_multi_fxn <- function(inputs,filename,ifim_site_name,ifim_metric){
                           "flow reduced by 30%",
                           "flow reduced by 40%",
                           "flow reduced by 50%"
-                          ), 
-         pch=1,  
+                          ),
+         pch=1,
          col=c("blue",
                "green",
                "firebrick",
@@ -401,8 +401,8 @@ hab_ts_plot_multi_fxn <- function(inputs,filename,ifim_site_name,ifim_metric){
 
   #plot flow time series legend (position 4)
   par(mar=c(0,0,0,0))
-  #plot(flow.ts.range_0[,1], rownames(wua.at.q_0), type="n", axes=FALSE, 
-  plot(flow.ts.range_0[,1], rownames(wua.at.q_0), type="n", axes=FALSE, 
+  #plot(flow.ts.range_0[,1], rownames(wua.at.q_0), type="n", axes=FALSE,
+  plot(flow.ts.range_0[,1], rownames(wua.at.q_0), type="n", axes=FALSE,
        xlab=NA, ylab=NA)
   legend("left", legend=c("gage flow",
                           "reduced by 5%",
@@ -411,8 +411,8 @@ hab_ts_plot_multi_fxn <- function(inputs,filename,ifim_site_name,ifim_metric){
                           "reduced by 30%",
                           "reduced by 40%",
                           "reduced by 50%"
-                          ), 
-         lty=1,  
+                          ),
+         lty=1,
          col=c("blue",
                "green",
                "firebrick",
@@ -421,7 +421,7 @@ hab_ts_plot_multi_fxn <- function(inputs,filename,ifim_site_name,ifim_metric){
                "black",
                "violet"
               ))
-  
+
 }
 
 #-------------------
